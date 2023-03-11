1223d0b7576d13b06eb0f46969f8f86b651cc53c
vitalspace <myglobalsounds@gmail.com>
2023-03-11 10:59:13 -0600

    Updated

--- index.ts
+++ index.ts
@@ -14,33 +14,7 @@ const person: Person = {
   address: "USA",
 };
 
-const result = await tool.createFile("./mundo/example.json", JSON.stringify(person));
-console.log(result)
-
-
-
-// const result = await tool.exists("./example.json");
-// result === true
-//   ? console.log("existe")
-//   : await tool.createFile("./example.json", JSON.stringify(person));
-
-// if (await tool.exists("./example.json")) {
-//   console.log("existe");
-// } else {
-
-//   const result = await tool.createFile("./example.json", JSON.stringify(person));
-//   console.log(result);
-// }
-
-// if (await tool.exists("./example.txt")) {
-//   console.log("existe");
-// } else {
-//   console.log("no extiste");
-// }
-
-// try {
-//   const result = await tool.createFile("./example.txt");
-//   console.log(result);
-// } catch (error) {
-//   console.log(error);
-// }
+const result = await tool.exists("./example.json");
+result === true
+  ? console.log("existe")
+  : await tool.createFile("./example.json", JSON.stringify(person));
